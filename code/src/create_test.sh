24525f8c7f3d9026b8eae6171863bee4b623365e
vicky <rhyme_yang@live.cn>
2017-12-29 17:47:34 +0800

    add data

--- code/src/create_test.sh
+++ code/src/create_test.sh
@@ -1,3 +1,3 @@
-sed -n '1,698 p'  org_shanghai_china.osm > shanghai_china.osm
-sed -n '4611958,4612292 p' org_shanghai_china.osm >> shanghai_china.osm 
+head -n 1000 org_shanghai_china.osm > shanghai_china.osm
+sed -n '4611958,4613211 p' org_shanghai_china.osm >> shanghai_china.osm 
 tail -n 47 org_shanghai_china.osm >> shanghai_china.osm 
